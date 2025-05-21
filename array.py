dea7b213888c1a3b3a0f8a1531509cf00fbf11aa
kiyaan-coder <kiyaanprozz@gmail.com>
2025-05-21 18:35:52 +0530

    changes

--- array.py
+++
@@ -1,5 +0,0 @@
-number1=int(input("enter the number1 :  "))
-number2=int(input("enter the number2 :  "))
-
-array=[number1 + number2,number1 - number2,number1 * number2,number1 % number2]
-print("the addition,subtraction,multiplication and division are   :   ",array)
---
+++ array_1.py
@@ -0,0 +1,8 @@
+# number1=int(input("enter the number1 :  "))
+# number2=int(input("enter the number2 :  "))
+
+# array2=[number1 + number2,number1 - number2,number1 * number2,number1 % number2]
+array1=[2,3,4,5]
+array1.append(3)
+print(array1)
+# print("the addition,subtraction,multiplication and division are   :   ",array)
