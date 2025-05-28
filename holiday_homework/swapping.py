f3993a6a022b3319832df6a3af9596e1126b6904
kiyaan-coder <kiyaanprozz@gmail.com>
2025-05-28 15:59:48 +0530

    swapping solution

---
+++ holiday_homework/swapping.py
@@ -0,0 +1,24 @@
+#using extra variables
+a = 5
+b = 10
+print("Before swapping : a =", a, ", b =", b)
+temp = a
+a = b
+b = temp
+print("After swapping : a =", a, ", b =", b)
+ 
+ #python way
+x = 15
+y = 20
+print("\nBefore swapping : x =", x, ", y =", y)
+x, y = y, x
+print("After swapping : x =", x, ", y =", y)
+
+#no extra variables
+m = 25
+n = 30
+print("\nBefore swapping : m =", m, ", n =", n)
+m = m + n
+n = m - n
+m = m - n
+print("After swapping : m =", m, ", n =", n)
