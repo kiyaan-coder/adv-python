470e345b27f12fca8ba91b743bb1b038a1b127b6
kiyaan-coder <kiyaanprozz@gmail.com>
2025-12-07 15:35:48 +0530

    nestedif file

---
+++ more loops/nestedif.py
@@ -0,0 +1,10 @@
+a = int(input("enter the percantage  :  "))
+
+if a >= 80:
+    print("got the A")
+elif a >= 50 :
+    print("you got th b")
+elif a>=35 :
+    print("you got C")
+else :
+   print("you are failed ")
--- more loops/while loop.py
+++ more loops/while loop.py
@@ -1,11 +1,9 @@
-for i in range(10,0,-1):
-    print(i)
 
 i = 1
 while i <=10:
     print(i) 
     i=i+1
 
-n = 10
-for i in range(1,11):
+n = int(input("enter the vlue :  "))
+for i in range(1,251):
     print(n , "x" , i , "=" , n*i)
